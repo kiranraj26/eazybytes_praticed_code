8900566b4f64f7831f84771ef1ff08090dd35356
kiran <kiranvijay026@gmail.com>
2024-11-29 19:55:21 +0530

    Wiring bean using method parameter

--- src/main/java/example/config/ProjectConfig.java
+++ src/main/java/example/config/ProjectConfig.java
@@ -16,10 +16,10 @@ public class ProjectConfig {
     }
 
     @Bean
-    public Person person() {
+    public Person person(Vehicle vehicle) {
         Person person = new Person();
         person.setName("Lucy");
-        person.setVehicle(vehicle());
+        person.setVehicle(vehicle);
         return person;
     }
 
